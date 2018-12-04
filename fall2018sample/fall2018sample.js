9fd6f2b9ed7cb66f11095a530193a528534117ea
Gan Lin <gxl1577@rit.edu>
2018-12-04 00:06:03 -0500

    practical sample

--- fall2018sample/fall2018sample.js
+++ fall2018sample/fall2018sample.js
@@ -1,1 +1,27 @@
 /* Don't use <script> tags in a linked js file! */
+
+
+let menuitem= document.querySelector("nav .menubox:nth-of-type(2) .menuitem");
+let submenu= document.querySelector("nav .menubox:nth-of-type(2) .menuitem .submenu");
+
+
+//menuitem.onclick = function (e) {
+//            submenu.style.visibility = "visible";
+//            //submenu.style.display = "block";
+//        }
+
+$(".menuitem").click(function (){
+    $(".submenu").slideToggle("slow");
+
+});
+
+
+
+let verse = ("content1.txt") // sets default verse element
+        $("#choose-content").val(verse); // changes menu option to default
+        $("#content").load(verse); // retrieves only default element
+
+        $("#choose-content").change(function() {
+            verse = $(this).val();
+            $("#content").load(verse);
+        });
