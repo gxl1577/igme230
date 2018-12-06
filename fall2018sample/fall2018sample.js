7c76c80fc1a73766797a2be889b5acf44b7cde00
Gan Lin <gxl1577@rit.edu>
2018-12-06 17:02:34 -0500

    final portfolio stuff

--- fall2018sample/fall2018sample.js
+++ fall2018sample/fall2018sample.js
@@ -1,8 +1,8 @@
 /* Don't use <script> tags in a linked js file! */
 
 
-let menuitem= document.querySelector("nav .menubox:nth-of-type(2) .menuitem");
-let submenu= document.querySelector("nav .menubox:nth-of-type(2) .menuitem .submenu");
+//let menuitem= document.querySelector("nav .menubox:nth-of-type(2) .menuitem");
+//let submenu= document.querySelector("nav .menubox:nth-of-type(2) .menuitem .submenu");
 
 
 //menuitem.onclick = function (e) {
@@ -10,18 +10,29 @@ let submenu= document.querySelector("nav .menubox:nth-of-type(2) .menuitem .subm
 //            //submenu.style.display = "block";
 //        }
 
+//$(".menuitem:first-of-type").click(function (){
+//    $(".submenu:first-of-type").slideToggle("slow");
+//
+//});
+//
+//$(".menuitem").next().click(function (){
+//    $(".submenu").next().slideToggle("slow");
+//
+//});
+//
+
 $(".menuitem").click(function (){
-    $(".submenu").slideToggle("slow");
+    $(this).next(".submenu").slideToggle("slow");
 
 });
 
 
 
-let verse = ("content1.txt") // sets default verse element
-        $("#choose-content").val(verse); // changes menu option to default
-        $("#content").load(verse); // retrieves only default element
+        let content = ("content1.txt"); // sets default content element
+        $("#choose-content").val(content); // changes menu option to default
+        $("#content").load(content); // retrieves only default element
 
         $("#choose-content").change(function() {
-            verse = $(this).val();
-            $("#content").load(verse);
+            content = $(this).val();
+            $("#content").load(content);
         });
