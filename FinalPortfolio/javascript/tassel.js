589ac71f73044e801fa454fca9c7f817eb6955c3
Gan Lin <gxl1577@rit.edu>
2018-12-10 07:36:33 -0500

    added an interactive prototype

--- FinalPortfolio/javascript/tassel.js
+++ FinalPortfolio/javascript/tassel.js
@@ -1,6 +1,7 @@
         //Modified script from original https://codepen.io/desirecode/pen/MJPJqV
         $(document).ready(function() {
             $(window).scroll(function() {
+				//tassel appears after 500 doc length from top
                 let percentage = $(this).scrollTop()/$(document).height()*100+10;
                 if ($(this).scrollTop() > 500) {
                     $('.tassel').fadeIn();
@@ -12,6 +13,8 @@
                     $('.rope').fadeOut();
                 }
             });
+
+			//click on tassel to go back to top
             $('.tassel').click(function() {
                 $("html, body").animate({
                     scrollTop: 0
